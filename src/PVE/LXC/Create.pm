pve-loong64-port/pve-container

commit f7b2a27ef94c0b382d35c9e03ed64884b8460017

oci create: allow tabs in oci config
it is fairly common to use tabs in oci configs. for example, the php
oci image from docker uses them quite extensively [1]. so exclude them
from the control characters we check for.

[1]:
https://github.com/docker-library/php/blob/8e01ffe94ed4ccca7c0a37be6dcb45995cf5d93b/8.3/trixie/apache/Dockerfile#L20

Signed-off-by: Shannon Sterz <s.sterz@proxmox.com>
Link: https://lore.proxmox.com/20251118110258.103565-1-s.sterz@proxmox.com
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

src/PVE/LXC/Create.pm
@@ -682,7 +682,7 @@ sub restore_oci_archive {
     );
 
     # should we rather validate this on the rust side already?
-    my $has_ctrl_char = sub { return $_[0] =~ /[\x00-\x1F\x7F]/; };
+    my $has_ctrl_char = sub { return $_[0] =~ /[\x00-\x08\x10-\x1F\x7F]/; };
     my $oci_config_get_checked_scalar = sub {
         my ($key) = @_;
         my $value = $unsafe_oci_config->{$key} // return;

commit 9658a90fd0daf69895c614ac88f565328d0aa932

config: only skip tab as control char for oci images
tab is 0x09, the next character is 0x0a not 0x10.

this follows:
 2785bef ("config: correctly skip tab characters in env/entrypoint property")
which fixed this for the lxc config parsing, but missed this site.

Fixes: f7b2a27 ("oci create: allow tabs in oci config")
Signed-off-by: Stoiko Ivanov <s.ivanov@proxmox.com>
Link: https://lore.proxmox.com/20260827160858.623104-2-s.ivanov@proxmox.com

src/PVE/LXC/Create.pm
@@ -683,7 +683,7 @@ sub restore_oci_archive {
     );
 
     # should we rather validate this on the rust side already?
-    my $has_ctrl_char = sub { return $_[0] =~ /[\x00-\x08\x10-\x1F\x7F]/; };
+    my $has_ctrl_char = sub { return $_[0] =~ /[\x00-\x08\x0A-\x1F\x7F]/; };
     my $oci_config_get_checked_scalar = sub {
         my ($key) = @_;
         my $value = $unsafe_oci_config->{$key} // return;